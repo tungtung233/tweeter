3d917c74ea1f8e2293a39f831678e14db92d7257
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-14 18:06:25 +0000

    add 'renderTweets' function - tweets can be read from an array

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,29 +1,35 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 
 $(document).ready(function () {
   
-  const createTweetElement = function (tweetData) {
+
+  const createTweetElement = function (tweet) {
     
-    const {user, content, created_at} = tweetData;
+    const {user, content, created_at} = tweet;
 
     const tweetDate = timeago.format(created_at)
 
@@ -60,10 +66,20 @@ $(document).ready(function () {
     return html;
   }
 
-  const $tweet = createTweetElement(tweetData);
-  console.log("tweet:", $tweet);
-  $('.tweets-container').append($tweet);
-  
+
+  const renderTweets = function (tweets) {
+    
+    const tweetsContainer = $('.tweets-container');
+
+    tweets.forEach(tweet => {
+      $tweet = createTweetElement(tweet);
+      tweetsContainer.append($tweet)
+    })
+
+  }
+
+  renderTweets(data)
+
   
 });
 
